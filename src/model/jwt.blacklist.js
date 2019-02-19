dd83704df19876604d1fefc4ba91a23cbd46fd04
BENRANDJAKRAM <fa_benrandja@esi.dz>
2019-02-19 18:15:16 +0100

    add unique token in token schema

--- src/model/jwt.blacklist.js
+++ src/model/jwt.blacklist.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const tokenSchema = new Schema({
-   token: {type: String, required: true, expires: "1d"}
+   token: {type: String, required: true, expires: "1d", unique: true}
 });
 
 const TokenModel = mongoose.model("token", tokenSchema);
