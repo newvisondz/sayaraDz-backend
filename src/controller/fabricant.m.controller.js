6800a80b3926b5b9948a4a1445054500dfde0490
BENRANDJAKRAM <fa_benrandja@esi.dz>
2019-02-25 22:09:55 +0100

    add create and delete single fabricant

--- src/controller/fabricant.m.controller.js
+++ src/controller/fabricant.m.controller.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const permissionController = require("./permission.controller");
 const Fabricant = require("../model/fabricant.model");
+const ObjectId = require("mongoose").Types.ObjectId;
 
 function index() {
     return [
@@ -10,14 +11,60 @@ function index() {
     ]
 }
 
+function create() {
+    return [
+        permissionController.isAdmin,
+        createFabricant
+    ]
+}
+function deleteOne() {
+    return [
+        permissionController.isAdmin,
+        deleteFabricant
+    ]
+}
+
+
 function listAll(req, res, next) {
     const query = Fabricant.getQueryObject(req.query);
     Fabricant.find(query)
         .limit(parseInt(req.query.limit))
         .select(req.query.select)
         .sort(req.query.sort)
-        .then(fabricants=> res.json(fabricants))
-        .catch(err=> res.json(err))
+        .then(fabricants => res.json(fabricants))
+        .catch(err => res.json(err))
 }
 
-module.exports = {index};
+function createFabricant(req, res, next) {
+    const fab = {
+        marque: req.body.marque
+    };
+    if (!fab.marque)
+        return res.json({
+            error: true,
+            msg: "empty marque field"
+        });
+
+    new Fabricant(fab).save()
+        .then(newFab=> res.json(newFab))
+        .catch(err=> {
+            res.json(err);
+            next(err);
+        })
+
+}
+
+async function deleteFabricant(req, res, next){
+    const isValid = ObjectId.isValid(req.params.id);
+    if(!isValid)return res.json({
+        error: true,
+        msg: "bad fabricant id"
+    });
+    const fab = {
+        _id: req.params.id
+    };
+    const result = await  Fabricant.deleteOne(fab);
+    res.json(result)
+}
+
+module.exports = {index, create, deleteOne};
--- src/routes/fabricant.m.routes.js
+++ src/routes/fabricant.m.routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const controller = require("../controller/fabricant.m.controller");
 
 router.get("/", controller.index());
-
+router.post("/", controller.create());
+router.delete("/:id", controller.deleteOne());
 module.exports = router;
