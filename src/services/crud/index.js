cfe9237aa3c2c05e8ad2a1aacff3bbb3eb511257
BENRANDJAKRAM <fa_benrandja@esi.dz>
2019-03-12 19:10:41 +0100

    add generic CRUD service

---
+++ src/services/crud/index.js
@@ -0,0 +1,85 @@
+const ObjectId = require('mongoose').Types.ObjectId
+
+function handleIdParams (_id, res) {
+  const isValid = ObjectId.isValid(_id)
+  if (!isValid) {
+    res.json({
+      error: true,
+      msg: 'bad ID'
+    })
+  }
+  return isValid
+}
+
+module.exports = (Model, name, filter) => ({
+
+  read: async ({ query, options: { limit, skip, sort } }, res, next) => {
+    try {
+      const result = await Model
+        .find(query)
+        .skip(skip)
+        .limit(limit)
+        .sort(sort)
+        .exec()
+      const count = await Model.countDocuments(query)
+      res.json({
+        [name + 's']: result,
+        count
+      })
+      next()
+    } catch (error) {
+      res.json(error)
+      next(error)
+    }
+  },
+
+  create: async ({ body }, res, next) => {
+    try {
+      const newModel = await new Model(body).save()
+      res.json(newModel)
+      next()
+    } catch (error) {
+      if (error.code == 11000) {
+        return res.json({
+          error: 1,
+          msg: 'duplicate ' + name
+        })
+      }
+      res.json(error)
+      next(error)
+    }
+  },
+
+  update: async ({ params, body }, res, next) => {
+    const { id } = params
+    if (!handleIdParams(id, res)) return
+    try {
+      const newFilter = {
+        _id: id,
+        ...filter
+      }
+      // Object.assign(body, additionalsOpts)
+      const result = await Model.updateOne(newFilter, body)
+      res.json(result)
+      next()
+    } catch (error) {
+      res.json(error)
+      next(error)
+    }
+    next()
+  },
+
+  deleteOne: async ({ params, body }, res, next) => {
+    const _id = params.id
+    if (!handleIdParams(_id, res)) return
+    try {
+      const result = await Model.deleteOne({ _id, ...body })
+      res.json(result)
+      next()
+    } catch (error) {
+      res.json(error)
+      next(error)
+    }
+  }
+
+})
