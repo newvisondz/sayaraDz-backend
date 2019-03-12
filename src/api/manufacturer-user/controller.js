1f2868ab2cc3cabdd062bf00497c4e7a6e90a6f6
BENRANDJAKRAM <fa_benrandja@esi.dz>
2019-03-12 21:09:29 +0100

    use generic crud service in manufacturer user routes

--- src/api/manufacturer-user/controller.js
+++ src/api/manufacturer-user/controller.js
@@ -1,103 +1,43 @@
 const { authenticated, isFabricantAdmin } = require('../../services/acl')
-const ManufacturerUser = require('./model')
-const ObjectId = require('mongoose').Types.ObjectId
+const ManufacturerUser = require('../manufacturer-user/model')
 const Validation = require('../../services/validation')
-
+const crud = require('../../services/crud')(ManufacturerUser, 'manufacturer_user', { isAdmin: false })
 const validate = new Validation(ManufacturerUser.schema)
-exports.index = [
+
+exports.read = [
   isFabricantAdmin,
   authenticated,
   validate.filter.bind(validate),
-  async ({ query, options: { limit, skip, sort } }, res) => {
-    try {
-      const filter = {
-        ...query,
-        isAdmin: false
-      }
-      const users = await ManufacturerUser
-        .find(filter)
-        .skip(skip)
-        .limit(limit)
-        .sort(sort)
-        .exec()
-      const count = await ManufacturerUser.countDocuments(filter)
-      res.json({
-        users,
-        count
-      })
-    } catch (error) {
-      res.json(error)
-    }
-  }
+  queryAdmin,
+  crud.read
 ]
 
 exports.create = [
   isFabricantAdmin,
   authenticated,
   validate.requirePaths.bind(validate),
-  async ({ body }, res) => {
-    try {
-      const admin = await new ManufacturerUser({
-        ...body,
-        isAdmin: false
-      }).save()
-      res.json(admin)
-    } catch (error) {
-      if (error.code == 11000) {
-        return res.json({
-          error: 1,
-          msg: 'duplicated manufacturer admin'
-        })
-      }
-      res.json(error)
-    }
-  }
+  bodyAdmin,
+  crud.create
 ]
 
 exports.update = [
   isFabricantAdmin,
   authenticated,
-  async ({ params, body }, res) => {
-    const { id } = params
-    if (!handleIdParams(id, res)) return
-    const result = await ManufacturerUser.updateOne({ _id: id }, body)
-    res.json(result)
-  }
+  validate.filter.bind(validate),
+  bodyAdmin,
+  crud.update
 ]
-
 exports.deleteOne = [
   isFabricantAdmin,
   authenticated,
-  async (req, res) => {
-    const _id = req.params.id
-    if (!handleIdParams(_id, res)) return
-    try {
-      const result = await ManufacturerUser.deleteOne({ _id })
-      res.json(result)
-    } catch (error) {
-      res.json(error)
-    }
-  }
+  bodyAdmin,
+  crud.deleteOne
 ]
-
-function handleIdParams (_id, res) {
-  const isValid = ObjectId.isValid(_id)
-  if (!isValid) {
-    res.json({
-      error: true,
-      msg: 'bad ID'
-    })
-  }
-  return isValid
+function bodyAdmin (req, res, next) {
+  req.body.isAdmin = false
+  next()
 }
-
-// new ManufacturerUser({
-//   email: 'akram@gmail.dz',
-//   school: 'esi',
-//   password: 'root',
-//   isAdmin: false,
-//   manufacturer: '5c744a029e7aaf23b87381ad'
-// })
-//   .save()
-//   .then(console.log)
-//   .catch(console.log)
+function queryAdmin (req, res, next) {
+  req.query.isAdmin = false
+  next()
+}
--- src/api/manufacturer-user/index.js
+++ src/api/manufacturer-user/index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const { login, logout } = require('../../services/auth')
-const { index, create, deleteOne, update } = require('./controller')
+const { read, create, deleteOne, update } = require('./controller')
 
 router.post('/login', login('local-manufacturer'))
 router.delete('/logout', logout)
 
-router.get('/', index)
+router.get('/', read)
 router.post('/', create)
 router.delete('/:id', deleteOne)
 router.put('/:id', update)
