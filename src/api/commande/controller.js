851b4ece69425d322cf5c13b4bb6f50c0b05c684
Harouit Chemseddine <fc_harouit@esi.dz>
2019-06-18 14:01:06 +0100

    Token handling while creating commande

--- src/api/commande/controller.js
+++ src/api/commande/controller.js
@@ -1,20 +1,26 @@
 const Commande = require('./model')
+const { isAutomobiliste, authenticated } = require('../../services/acl')
 
-exports.create = (req, res) => {
-  const body = req.body
-  console.log(body.montant)
-  let commande = new Commande({
-      montant: body.montant,
-      date: body.date,
-      automobiliste: body.automobiliste,
-      vehicule: body.vehicule
-    }
-  );
-  commande.save((err, commande)=>{
-    if (err) res.send("erreur lors de la création de la commande : " + err);
-    res.send(commande);
-  })
-};
+exports.create =[
+  isAutomobiliste,
+  authenticated,
+  (req, res) => {
+    const body = req.body
+    console.log(body.montant)
+    let commande = new Commande({
+        montant: body.montant,
+        date: body.date,
+        automobiliste: body.automobiliste,
+        vehicule: body.vehicule
+      }
+    );
+    commande.save((err, commande)=>{
+      if (err) res.send("erreur lors de la création de la commande : " + err);
+      res.send(commande);
+    })
+  }
+] 
+
 
 
 exports.show = (req, res) => {
@@ -26,15 +32,15 @@ exports.show = (req, res) => {
 
 exports.update = function (req, res) {
   let updatedCommande = req.body;
-  Commande.findByIdAndUpdate(req.params.id, updatedCommande, { "new": true}, (err, commande) => {
+  Commande.findOneAndUpdate(req.params.id, updatedCommande, { "new": true}, (err, commande) => {
         if (err) res.send("erreur lors du update de la commande : " + err);
         res.send(commande);
   });
 };
 
 exports.deleteOne = function (req, res) {
-  Commande.findByIdAndDelete(req.params.id,(err) => {
-        if (err) res.send("erreur lors de la delete de la commande : " + err);
+  Commande.findOneAndDelete(req.params.id,(err) => {
+        if (err) res.send("erreur lors du delete de la commande : " + err);
         res.send("Commande supprimée avec succès");
   });
 };
