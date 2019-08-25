7f0117d2a6bc0bd65469de72dc7ccdb389ff38d0
BENRANDJAKRAM <fa_benrandja@esi.dz>
2019-08-25 11:48:04 +0100

    update tarifs module

--- src/api/tarifs/resolvers.js
+++ src/api/tarifs/resolvers.js
@@ -1,20 +1,28 @@
-const moment = require('moment')
 const Model = require('./model')
 
-exports.getPrice = async document => {
-  let res = await Model.find({ document }).sort('createdAt')
-  res = res.filter(
-    d => moment().isSameOrAfter(d.start) && moment().isSameOrBefore(d.end)
-  )
+const getPrice = async document => {
+  let res = await Model.find({
+    document,
+    start: {
+      '$lte': new Date()
+    },
+    end: {
+      '$gte': new Date()
+    }
+  }).sort('-createdAt')
 
-  console.log({ res })
   return (res[0] && res[0].amount) || 0
 }
 
-exports.getTotalPrice = async (...documents) => {
-  const tarifs = await Promise.all(documents.map(d => this.getPrice(d)))
+const getTotalPrice = async (...documents) => {
+  const tarifs = await Promise.all(documents.map(d => getPrice(d)))
   console.log({ documents })
   return tarifs.reduce(
     (previous, current) => previous + current, 0
   )
+}
+
+module.exports = {
+  getPrice,
+  getTotalPrice
 }
